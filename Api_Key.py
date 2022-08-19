ba43bed526029109a04a32cbdd46646294d4dd29
Omkar Jadhav <106991458+FatGuyy@users.noreply.github.com>
2022-08-19 10:33:25 +0530

    uploaded files.

---
+++ Api_Key.py
@@ -0,0 +1,2 @@
+#Get an api key for unsplash api & paste it here
+key = 'apSPB1KMGBwcHlPV2x3hnG8PXWo93EVDuzP8RrwXMlI'
---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+import Api_Key
+import os
+
+
+#Enter the terms you want to search in this list
+#---------------------------------------------
+
+searchs = ['minecraft', 'ghost', 'hero']
+
+#---------------------------------------------
+
+
+#Making Directories for the photos and if exists already skipping
+for search in searchs:
+    path = fr'.\{search}'
+    if os.path.exists(path):
+        continue
+    os.mkdir(search)
+
+#download function using given url
+def download(down_url, i, dir_name):
+    r = requests.get(down_url)
+    with open(f'.\{dir_name}\{i}.png','wb') as f:
+        f.write(r.content)
+
+#iterating the searchs and scrapping the reccieved data
+for search in searchs:
+    url = f'https://api.unsplash.com/search/photos?client_id={Api_Key.key}&per_page=11&query={search}'
+    response = requests.get(url)
+    #file_url = (list(list(list(list(res.json().items())[2][1])[0].values())[10].values())[0])
+    for i in range(11):
+        file_url = (list(list(list(list(response.json().items())[2][1])[i].values())[10].values())[0])  #Link to download
+        download(file_url, i, search)
